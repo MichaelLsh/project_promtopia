f1af677b66b85dcaab890b2513f5829f3c76e390
MichaeLsh <lsh20011018@gmail.com>
2024-08-26 23:31:13 -0600

    more lower case p fixed

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const router = useRouter(); // get the router object
